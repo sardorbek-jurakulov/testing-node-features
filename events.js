61196d8228d09352a64005467fa307a938752775
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-14 20:55:45 +0500

    WIP

--- events.js
+++ events.js
@@ -1,9 +1,17 @@
 const EventEmitter = require('events');
+const fs = require('fs');
 
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function() {
-  console.log('Listener chaqirildi');
+emitter.on('messageLogged', function(arg) {
+  console.log('Listener chaqirildi', arg);
 })
 
 emitter.emit('messageLogged', {id: 1, url: 'http://...'});
+
+fs.writeFile('./events-ht.js', '', function(err) {
+  if(err) {
+    throw err;
+  }
+  console.log("file created successful");
+});
