fc7904b616e8be93e6d45b13ce27c523fc9204a2
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-14 21:23:14 +0500

    WIP

--- events.js
+++ events.js
@@ -1,17 +1,8 @@
-const EventEmitter = require('events');
-const fs = require('fs');
+const Logger = require('./logger.js');
+const logger = new Logger();
 
-const emitter = new EventEmitter();
-
-emitter.on('messageLogged', function(arg) {
+logger.on('messageLogged', function(arg) {
   console.log('Listener chaqirildi', arg);
-})
-
-emitter.emit('messageLogged', {id: 1, url: 'http://...'});
-
-fs.writeFile('./events-ht.js', '', function(err) {
-  if(err) {
-    throw err;
-  }
-  console.log("file created successful");
 });
+
+logger.log('message');
--- logger.js
+++ logger.js
@@ -1,9 +1,19 @@
-const url = 'http:192.168.7.1/logger';
+// const url = 'http:192.168.7.1/logger';
 
-function log(message) {
-  // to-do: bu yerda message'ni url'ga http post qilib jo'natish kodi bo'lishi kerak
-  console.log(message);
+// function log(message) {
+//   // to-do: bu yerda message'ni url'ga http post qilib jo'natish kodi bo'lishi kerak
+//   console.log(message);
+// }
+
+// module.exports = log;
+// console.log(module);
+
+const EventEmitter = require('events');
+class Logger extends EventEmitter {
+  log(message) {
+    console.log(message);
+    this.emit('messageLogged', {id: 1, url: 'http://...'});
+  }
 }
 
-module.exports = log;
-console.log(module);
+module.exports = Logger;
