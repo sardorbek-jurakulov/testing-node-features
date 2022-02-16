37ac41ca31d71c3409e8cf90900f42c486d0bdd7
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-16 10:51:32 +0500

    WIP

--- workWithHttpModule.js
+++ workWithHttpModule.js
@@ -1,4 +1,7 @@
 const http = require('http');
 const server = http.createServer();
+server.on('connection', (socket) => {
+  console.log("Yangi bog'lanish...");
+});
 server.listen(8000);
-console.log(`${server.address().port} - portni eshitishni boshladik!`);
+console.log(`${server.address().port} - portni eshitishni boshladim!`);
